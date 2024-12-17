93bb6bb63fb49c43c8d7277b05ead3da082731f5
William Li <williamli1@fwd.com>
2024-12-17 14:06:12 +0800

    add login page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 import { HomeLayout } from "@layouts/HomeLayout";
 import { DayPlanPage } from "./pages/DayPlanPage";
 import { HomePage } from "./pages/HomePage";
+import { LoginPage } from "./pages/LoginPage";
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 				<Route index element={<HomePage />} />
 				<Route path="day_plan" element={<DayPlanPage />} />
 			</Route>
+			<Route path="/login" element={<LoginPage />} />
 		</Routes>
 	);
 }
