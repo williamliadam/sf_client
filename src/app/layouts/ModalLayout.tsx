8642d3a200f1ad9997ab01b45c2477a80fdb37a8
William Li <williamli1@fwd.com>
2024-12-17 14:56:15 +0800

    biome ignore node_modules

--- src/app/layouts/ModalLayout.tsx
+++ src/app/layouts/ModalLayout.tsx
@@ -20,14 +20,7 @@ export function ModalLayout({
 					className={`bg-orange-400 w-fit h-fit p-6 rounded-md shadow-md flex flex-col transition-all ease-in-out duration-1000 ${isOpen ? "opacity-100" : "opacity-0 "}`}
 				>
 					<div className=" text-lg text-white">Modal Title</div>
-					<div className=" py-6 max-w-xl">
-						Eiusmod anim deserunt reprehenderit minim sint pariatur sunt culpa
-						ea aute aliquip. Mollit minim commodo veniam tempor do nostrud
-						dolor. Cupidatat aute minim sint adipisicing minim aliqua laborum
-						duis nulla nisi. Aute cupidatat quis laborum culpa enim cillum.
-						Excepteur fugiat amet fugiat deserunt. Deserunt officia deserunt ea
-						mollit ea.
-					</div>
+					<div className=" py-6 max-w-xl">Hello</div>
 					<div className=" w-full flex flex-row justify-end">
 						<button
 							type="button"
